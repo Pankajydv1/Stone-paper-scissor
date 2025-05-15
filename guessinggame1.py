917a3d36f390276fc650364075a33280222e1f0d
Pankaj Yadav <proceedtopankaj2@gmail.com>
2025-05-15 15:28:13 +0545

    stone Paper Scissor

---
+++ guessinggame1.py
@@ -0,0 +1,33 @@
+import random
+
+while True:
+    print('''
+ 1 - 100 : Choose number to play game
+  101    : End game  
+ ''')
+    c = random.randint(1, 100)
+    i = int(input("enter your number:"))
+
+    if (i < c) and (i <= 100):
+        if c - i < 10:
+            print("your number is low")
+            print("computer number is", c)
+        else:
+            print("your number is too low")
+            print("computer number is", c)
+
+    elif (i > c) and (i <= 100):
+        if i - c < 10:
+            print("your number is high")
+            print("computer number is", c)
+        else:
+            print("your number is too high")
+            print("computer number is", c)
+
+    elif (i == c) and (i <= 100):
+        print("Both number match")
+        print("congrats you won !!")
+
+    elif i >= 101:
+        print("game end...")
+        break
---
+++ stonepaperscissors.py
@@ -0,0 +1,81 @@
+import random
+l=['Stone','Paper','Scissor']
+a=b=d =0
+e=1
+while True :
+    print("ROUND",e,"/5")
+    print("select your pref...:")
+    i=int(input('''
+    1  Stone
+    2  Paper
+    3  Scissor
+    '''))
+    c=random.choice(l)
+    a=a+1
+    e=e+1
+    if c=='Stone' :
+        if i==1 :
+            print("Draw...")
+            print("Both you and computer have Selected Stone..")
+            b= b + 1
+            d= d + 1
+        if i==2  :
+            print("Computer won")
+            print("Computer has selected Stone & You have selected Scissor..")
+            b = b + 1
+            d = d + 0
+        if i==3  :
+            print("You won")
+            print("Computer has selected Stone & You have selected Paper..")
+            b = b + 0
+            d = d + 1
+    if c == 'Paper':
+        if i == 2:
+            print("Draw...")
+            print("Both you and computer have Selected Paper..")
+            b = b + 1
+            d = d + 1
+        if i == 1:
+            print("Computer won")
+            print("Computer has selected Paper & You have selected Stone..")
+            b = b + 1
+            d = d + 0
+        if i == 3:
+            print("You won")
+            print("Computer has selected Paper & You have selected Scissor..")
+            b = b + 0
+            d = d + 1
+    if c == 'Scissor':
+        if i == 3:
+            print("Draw...")
+            print("Both you and computer have Selected Scissor..")
+            b = b + 1
+            d = d + 1
+        if i == 2:
+            print("Computer won")
+            print("Computer has selected Scissor & You have selected Paper..")
+            b = b + 1
+            d = d + 0
+        if i == 1:
+            print("You won")
+            print("Computer has selected Scissor & You have selected Stone..")
+            b = b + 0
+            d = d + 1
+    if a==5 :
+        print("THE FINAL RESULT AFTER 5 ROUND:")
+        if b==d :
+            print("Game Draw....")
+            print("your score is ",d)
+            print("Computer score is ",b)
+            break
+        if b>=d :
+            print("Computer Won....")
+            print("your score is ", d)
+            print("Computer score is ", b)
+            break
+        if b<=d :
+            print("You Won....")
+            print("your score is ", d)
+            print("Computer score is ", b)
+            break
+
